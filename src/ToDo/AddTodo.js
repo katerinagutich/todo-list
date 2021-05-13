7f34aa485f3b445d73ee126df23b3a78f0393a40
Kate <katerina_gutich@mail.ru>
2021-05-13 19:54:07 +0300

    Refactor form input

--- src/ToDo/AddTodo.js
+++ src/ToDo/AddTodo.js
@@ -1,25 +1,34 @@
 import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 
+function useInputValue(defaultValue = '') {
+    const [value, setValue] = React.useState(defaultValue)
+    return {
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+}
+
 function AddTodo({onCreate}) {
 
-    const [value, setValue] = React.useState('')
+    const input = useInputValue('')
 
     function submitHandler(event) {
         event.preventDefault()
 
-        if (value.trim()) {
-            onCreate(value)
-            setValue('')
+        if (input.value().trim()) {
+            onCreate(input.bind.value)
+            input.clear()
         }
     }
 
     return (
         <form onSubmit={submitHandler}>
-            <input
-                value={value}
-                onChange={event => setValue(event.target.value)}
-            />
+            <input {...input.bind} />
             <button type="submit">ADD</button>
         </form>
     )
